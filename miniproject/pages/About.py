8e40be6fb0cc79b0dd6be99fecf990017b07a468
Mrudula A P <122630243+mrudulamadhavan@users.noreply.github.com>
2024-01-23 02:41:06 +0530

    Update About.py

--- miniproject/pages/About.py
+++ miniproject/pages/About.py
@@ -14,20 +14,27 @@ dir_of_interest = os.path.join(PARENT_DIR, "resources")
 
 
 st.header(":green[Overview ]")
-st.write(' The integration of rental bike systems into urban landscapes has ushered in a new era of convenience and sustainability in transportation. These systems, characterized by automated processes and strategically placed kiosks across cities, provide users with flexible and eco-friendly options for short-distance travel. However, the seamless functioning of these bike-sharing programs relies heavily on the accurate prediction of bike demand at different hours. In this dynamic urban environment, anticipating the right number of bikes needed ensures a stable supply, minimizes waiting times, and optimizes the overall user experience. This introduction sets the stage for exploring the crucial role of predictive models in the success of rental bike systems and their impact on shaping contemporary urban mobility.')
+
+st.write('    The introduction of rental bikes in various urban areas has notably enhanced mobility comfort. It is imperative to ensure the timely availability and accessibility of rental bikes to minimize waiting times. ')
+st.write('The imperative of sustaining a stable supply of rental bikes in the city is a pressing concern. Accurately gauging bike demand at specific times is especially vital for bike rental companies.') 
+
+
 
 st.header(":green[Problem Statement ]")
-st.write(' The critical challenge in bike rental systems is the accurate prediction of demand at varying hours. This problem statement underscores the necessity for sophisticated predictive models to anticipate the required bike count, ensuring a stable supply and minimizing user wait times. Addressing this dynamic aspect of urban mobility, influenced by factors like historical patterns and weather conditions, is crucial for the effective functioning of bike-sharing programs, emphasizing the need for innovative solutions to enhance overall system performance and user satisfaction.')
+st.write(' The critical challenge in bike rental systems is the accurate prediction of demand at varying hours. This problem statement underscores the necessity for sophisticated predictive models to anticipate the required bike count, ensuring a stable supply and minimizing user wait times.')
+st.write('Addressing this dynamic aspect of urban mobility, influenced by factors like historical patterns and weather conditions, is crucial for the effective functioning of bike-sharing programs, emphasizing the need for innovative solutions to enhance overall system performance and user satisfaction.')
+st.write('Our primary goal is to address this challenge by creating a solution that forecasts bike demand, considering diverse factors such as city weather conditions and different times of the day.')
 
 st.write('Create a web application for bike-sharing demand prediction using Streamlit and Python')
     
 st.header(":green[About Dataset]")
 st.write("The Bike Sharing Demand dataset contains information about bike rental in Seoul from 2017-2018. It includes hourly observations of 14 attributes, such as the date, time, number of rented bikes, weather information (Temperature, Humidity, Windspeed, Visibility, Dewpoint, Solar radiation, Snowfall, Rainfall) and other factors that may influence bike rental demand.") 
+st.write('')
 IMAGE_PATH2 = os.path.join(dir_of_interest, "images", "dataset.png")
 img = image.imread(IMAGE_PATH2)
 st.image(img) 
 
-st.subheader( 'red[Target Column :] blue[Rented Bike Count]')
+st.subheader( 'Target Column : :red[Rented Bike Count]')
 
 DATA_PATH = os.path.join(dir_of_interest, "data", "BikeData.csv")
 df = pd.read_csv(DATA_PATH)
