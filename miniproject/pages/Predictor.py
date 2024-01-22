efbcf9b357ce4d5bc860d3d91f9e42e1d496efb1
Mrudula A P <122630243+mrudulamadhavan@users.noreply.github.com>
2024-01-23 03:18:53 +0530

    Update Predictor.py

--- miniproject/pages/Predictor.py
+++ miniproject/pages/Predictor.py
@@ -32,7 +32,9 @@ DATA_PATH1=os.path.join(DATA_PATH, "BikeData.csv")
 df=pd.read_csv(DATA_PATH1,encoding='latin')
 df1 = df.copy
 
-
+df['Date'] = pd.to_datetime(df['Date'], format="%d/%m/%Y")
+df['Month'] = df['Date'].dt.month.astype('category')
+df['Weekday'] = df['Date'].dt.day_name().astype('category')
 
 
 def prediction(season,month,weekday,hour,temperature,humidity,visibility,windspeed,solarrdn,rainfall,snowfall):
