5ab33894cf71bed956405477315a868eda97a50d
Mrudula A P <122630243+mrudulamadhavan@users.noreply.github.com>
2024-01-23 10:23:30 +0530

    Update Data Analysis.py

--- miniproject/pages/Data Analysis.py
+++ miniproject/pages/Data Analysis.py
@@ -3,10 +3,6 @@ from matplotlib import image
 import matplotlib.pyplot as plt
 import pandas as pd
 import pip
-import subprocess
-
-# Install seaborn using pip
-subprocess.run(["pip", "install", "seaborn"])
 import seaborn as sns
 import plotly.express as px
 import numpy as np
