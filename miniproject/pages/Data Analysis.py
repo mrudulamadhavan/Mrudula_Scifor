c3557b88949ddd7eba1b6911c2e1317c7ee4bf4b
Mrudula A P <122630243+mrudulamadhavan@users.noreply.github.com>
2024-01-23 12:26:29 +0530

    Update Data Analysis.py

--- miniproject/pages/Data Analysis.py
+++ miniproject/pages/Data Analysis.py
@@ -55,21 +55,21 @@ fig1 = px.bar(df, x='Hour', y='Rented Bike Count', color='Hour',title='Rented Bi
 st.plotly_chart(fig1, use_container_width=True)
 st.write("-----------------------------------------------------------------------------------")
 # Line plot for Average Rented Bike Count by Hour for each Season using Seaborn
-st.subheader('         Rented Bike Count Trend by Hour for Each Season')
+# st.subheader('         Rented Bike Count Trend by Hour for Each Season')
 plt.figure(figsize=(12, 6))
 sns.lineplot(x="Hour", y="Rented Bike Count", hue="Seasons", data=df)
 plt.ylabel("Avg Rented Bike Count")
-plt.title("Rented Bike Count Trend by Hour for Each Season")
+plt.title("Rented Bike Count Trend by Hour for Each Season",, fontsize=15)
 # Display the plot in the Streamlit app
 st.pyplot()
-st.write('The demand is higher during rush hour (i.e., 7-9AM and 5-7PM) when people go to offices/schools and come back in the evening. The Rentals Bike demand trend pattern is the same for all the seasons, only levels are different. Demand level in winter is the lowest and highest in Summer')
+st.write('The demand is higher during rush hour (i.e., 7-9 AM and 5-7 PM) when people go to offices/schools and come back in the evening. The Rentals Bike demand trend pattern is the same for all the seasons, only levels are different. Demand level in winter is the lowest and highest in Summer')
 st.write("--------------------------------------------------------------------------------")
 # Line plot for Average Rented Bike Count by Hour for each weekday using Seaborn
 st.subheader('          Rented Bike Count Trend by Hour for Weekdays ')
 plt.figure(figsize=(12, 6))
 fig = sns.lineplot(x="Hour", y="Rented Bike Count", hue="Weekday", data=df)
 plt.ylabel("Avg Rented Bike Count")
-plt.title("Rented Bike Count Trend by Hour for Weekdays")
+plt.title("Rented Bike Count Trend by Hour for Weekdays", fontsize=15)
 # Display the plot in the Streamlit app
 st.pyplot()
 st.write('The bike rental pattern of weekdays and weekends is different.In the weekend the demand becomes high in the afternoon and the demand for office timings is high during weekdays.')
