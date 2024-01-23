e0a757ca235724387a0975099dd8da4196d7443a
Mrudula A P <122630243+mrudulamadhavan@users.noreply.github.com>
2024-01-23 12:00:06 +0530

    Update Data Analysis.py

--- miniproject/pages/Data Analysis.py
+++ miniproject/pages/Data Analysis.py
@@ -44,8 +44,11 @@ col2.plotly_chart(fig_2, use_container_width=True)
 st.write('Rented Bike Count is right skewed. It means that most of the data falls on the lower end of the scale, and there are relatively fewer instances where a large number of bikes were rented.')
 st.write('--------------------------------------------------------------------------------------')
 feature = st.selectbox("Choose any feature to see the relationship with Rented Bike Count", ['Seasons', 'Month', 'Weekday', 'Hour'])
-fig = px.bar(df, x=feature, y='Rented Bike Count', color=feature)
-st.plotly_chart(fig, use_container_width=True)
+col1, col2 = st.columns(2)
+fig1 = px.bar(df, x=feature, y='Rented Bike Count', color=feature)
+col1.plotly_chart(fig1, use_container_width=True)
+fig2 = px.box(df, x=feature, y='Rented Bike Count', color=feature)
+col2.plotly_chart(fig2, use_container_width=True)
 st.write('---')
 
 
